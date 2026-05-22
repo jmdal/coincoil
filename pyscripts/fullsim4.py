21e9c5ebf538618338f3fdc9a6ba6d1543d52715
Joseph Martin Dalawangbayan <jm.dalawangbayan@outlook.com>
2026-05-22 08:29:03 +0800

    cad progress, minor simulation work

--- pyscripts/fullsim4.py
+++ pyscripts/fullsim4.py
@@ -7,6 +7,7 @@ from math import sqrt, log10
 import csv
 
 CuRes = 1.71*10**-8 # Copper Resistivity
+CuDense = 8960
 
 class Coilsim:
     def __init__(self, wdia, bwid, bhei, lthi, turn, clen, capa, volt, scon):
@@ -22,15 +23,21 @@ class Coilsim:
 
         self.tpll = clen//wdia # Turns Per Layer (theoretically possible levels list)
         self.ttll = wirelengthrect(wdia, bwid+2*lthi, bhei+2*lthi, turn, self.tpll) # Total Length
+        self.flen = self.ttll/1000 # Total Length as meters
+
         self.lays = math.ceil(turn/self.tpll) # Layer Count
         self.fwid = self.bwid + 2*self.lthi + self.lays*(2*wdia) # Final Width
         self.fhei = self.bhei + 2*self.lthi + self.lays*(2*wdia) # Final Height
+
+
+        self.fmas = self.ttll * ((wdia/2000)**2) * math.pi * CuDense# Total Mass (g)
+
         self.cres = (CuRes*self.ttll) / ((wdia/2000)**2 * math.pi) # Total Resistance (milli )
 
         # Final Inductance???
 
         # let's try to use this: https://www.allaboutcircuits.com/tools/rectangle-loop-inductance-calculator/
-        self.cind = (turn**2) * (4 * 10**-7)*( -2*(bwid + bhei) + 2*math.sqrt(bwid**2 + bhei**2) - bhei*math.log((bhei + math.sqrt(bwid**2 + bhei**2))/bwid) - bwid*math.log((bwid + math.sqrt(bwid**2 + bhei**2))/bhei) + bhei*math.log(4*bhei/wdia) + bwid*math.log(4*bwid/wdia) ) * 1000 # x1000 correction factor
+        self.cind = (turn**2) * (4 * 10**-7)*( -2*(self.fwid + self.fhei) + 2*math.sqrt(self.fwid**2 + self.fhei**2) - bhei*math.log((self.fhei + math.sqrt(self.fwid**2 + bhei**2))/self.fwid) - self.fwid*math.log((self.fwid + math.sqrt(self.fwid**2 + self.fhei**2))/self.fhei) + bhei*math.log(4*self.fhei/wdia) + self.fwid*math.log(4*self.fwid/wdia) ) * 1000 # x1000 correction factor
 
         # Total Inductance (micro-henry)
 
@@ -92,10 +99,10 @@ def fullsim(parameters, spice_file=""):
         if prompt == "C":
             return 0
         elif prompt == "V":
-            print("Test [xx]\tWdia [mm]\tBwid [mm]\tBhei [mm]\tLthi [mm]\tTurn [xx]\tTpll [xx]\tClen [mm]\tScon [xx]\tTlen [mm]\tCapa [µF]\tVolt [1V]\tFwid [mm]\tFhei [mm]\tCres [mΩ]\tCind [µH]")
+            print("Test [xx]\tWdia [mm]\tBwid [mm]\tBhei [mm]\tLthi [mm]\tTurn [xx]\tTpll [xx]\tClen [mm]\tScon [xx]\tTlen [mm]\tCapa [µF]\tVolt [1V]\tFwid [mm]\tFhei [mm]\tFlen [1m]\tFmas [1g]\tCres [mΩ]\tCind [µH]")
             for i in range(len(parameters)):
                 pm = parameters[i]
-                print(f"{i+1:.0f}\t\t{pm.wdia:.2f}\t\t{pm.bwid:.2f}\t\t{pm.bhei:.2f}\t\t{pm.lthi:.2f}\t\t{pm.turn:.0f}\t\t{pm.tpll:.0f}\t\t{pm.clen:.2f}\t\t{pm.scon:.0f}\t\t{pm.clen*pm.scon:.2f}\t\t{pm.capa:.0f}\t\t{pm.volt:.2f}\t\t{pm.fwid:.2f}\t\t{pm.fhei:.2f}\t\t{pm.cres:.2f}\t\t{pm.cind:.2f}")
+                print(f"{i+1:.0f}\t\t{pm.wdia:.2f}\t\t{pm.bwid:.2f}\t\t{pm.bhei:.2f}\t\t{pm.lthi:.2f}\t\t{pm.turn:.0f}\t\t{pm.tpll:.0f}\t\t{pm.clen:.2f}\t\t{pm.scon:.0f}\t\t{pm.clen*pm.scon:.2f}\t\t{pm.capa:.0f}\t\t{pm.volt:.2f}\t\t{pm.fwid:.2f}\t\t{pm.fhei:.2f}\t\t{pm.flen:.2f}\t\t{pm.fmas:.2f}\t\t{pm.cres:.2f}\t\t{pm.cind:.2f}")
         elif prompt == "S":
             break
 
@@ -129,7 +136,7 @@ def fullsim(parameters, spice_file=""):
 
     print("Starting LTSpice...")
 
-    sim = PyLTSpice.SimRunner(output_folder="pyscripts/output/ltspice",simulator="D:\\ProgramFiles\\LTspice\\LTspice.exe",parallel_sims=10)
+    sim = PyLTSpice.SimRunner(output_folder="pyscripts/output/ltspice",simulator="D:\\ProgramFiles\\LTspice\\LTspice.exe",parallel_sims=8,timeout=10.00)
 
     net = PyLTSpice.SpiceEditor(spice_file)
 
@@ -148,6 +155,7 @@ def fullsim(parameters, spice_file=""):
 
         
         print(f"Finished sim {prog}/{len(parameters)}: \t[{i.capa:.4f}µF, {i.cind:.4f}µH, {i.cres:.4f}mΩ]")
+        #print(sim.okSim, sim.runno)
 
     sim.wait_completion()
     for raw, log in sim:
@@ -283,6 +291,8 @@ def fullsim(parameters, spice_file=""):
                 pm.volt,
                 pm.fwid,
                 pm.fhei,
+                pm.flen,
+                pm.fmas,
                 pm.cres,
                 pm.cind,
                 pm.curr,
@@ -297,10 +307,10 @@ def fullsim(parameters, spice_file=""):
             ])
             #writ.writerow([i+1, pm[0], pm[1], pm[2], pm[3], pm[6], tlns[i], pm[4], pm[5], dias[i], ress[i], inds[i], cnts[i], flds[i], frcs[i], accs[i], spds[i], engs[i]])
 
-    print("Test [xx]\tWdia [mm]\tBwid [mm]\tBhei [mm]\tLthi [mm]\tTurn [xx]\tTpll [xx]\tClen [mm]\tScon [xx]\tTlen [mm]\tCapa [µF]\tVolt [1V]\tFwid [mm]\tFhei [mm]\tCres [mΩ]\tCind [µH]\tCurr [1A]\tRise [ms]\tFall [ms]\tRech [ms]\tFild [1T]\tForc [1N]\tAcce [m/s2]\tSped [m/s]\tEngy [1J]")
+    print("Test [xx]\tWdia [mm]\tBwid [mm]\tBhei [mm]\tLthi [mm]\tTurn [xx]\tTpll [xx]\tClen [mm]\tScon [xx]\tTlen [mm]\tCapa [µF]\tVolt [1V]\tFwid [mm]\tFhei [mm]\tFlen [1m]\tFmas [1g]\tCres [mΩ]\tCind [µH]\tCurr [1A]\tRise [ms]\tFall [ms]\tRech [ms]\tFild [1T]\tForc [1N]\tAcce [m/s2]\tSped [m/s]\tEngy [1J]")
     for i in range(len(parameters)):
         pm = parameters[i]
-        print(f"{i+1}\t\t{pm.wdia:.2f}\t\t{pm.bwid:.2f}\t\t{pm.bhei:.2f}\t\t{pm.lthi:.2f}\t\t{pm.turn:.0f}\t\t{pm.tpll:.0f}\t\t{pm.clen:.2f}\t\t{pm.scon:.0f}\t\t{pm.scon*pm.clen:.2f}\t\t{pm.capa:.0f}\t\t{pm.volt:.1f}\t\t{pm.fwid:.2f}\t\t{pm.fhei:.2f}\t\t{pm.cres:.2f}\t\t{pm.cind:.2f}\t\t{pm.curr:.2f}\t\t{pm.rise:.3f}\t\t{pm.fall:.3f}\t\t{pm.rech:.2f}\t\t{pm.fild:.4f}\t\t{pm.forc:.4f}\t\t{pm.acce:.2f}\t\t{pm.sped:.4f}\t\t{pm.engy:.4f}")
+        print(f"{i+1}\t\t{pm.wdia:.2f}\t\t{pm.bwid:.2f}\t\t{pm.bhei:.2f}\t\t{pm.lthi:.2f}\t\t{pm.turn:.0f}\t\t{pm.tpll:.0f}\t\t{pm.clen:.2f}\t\t{pm.scon:.0f}\t\t{pm.scon*pm.clen:.2f}\t\t{pm.capa:.0f}\t\t{pm.volt:.1f}\t\t{pm.fwid:.2f}\t\t{pm.fhei:.2f}\t\t{pm.flen:.2f}\t\t{pm.fmas:.2f}\t\t{pm.cres:.2f}\t\t{pm.cind:.2f}\t\t{pm.curr:.2f}\t\t{pm.rise:.3f}\t\t{pm.fall:.3f}\t\t{pm.rech:.2f}\t\t{pm.fild:.4f}\t\t{pm.forc:.4f}\t\t{pm.acce:.2f}\t\t{pm.sped:.4f}\t\t{pm.engy:.4f}")
     #print(frcs)
 
 
@@ -314,30 +324,45 @@ def fullsim(parameters, spice_file=""):
 
 
 
-
+"""
 fullsim([Coilsim(Wdia, Bwid, Bhei, Lthi, Turn, Clen, Capa, Volt, Scon)
-         for Wdia in [awg(12), awg(14), awg(16)] # mm (+ AWG)
+         for Wdia in [awg(12), awg(14)] # mm (+ AWG)
          for Bwid in [24] # mm
          for Bhei in [3] # mm
          for Lthi in [i for i in range(2,6)] # mm
          for Turn in [i for i in range(25, 200+1, 25)] # count
-         for Clen in [30,40,50] # mm
+         for Clen in [50,60,70,80] # mm
          for Capa in [1000,10000] # uF
          for Volt in [63,90] # V
-         for Scon in [20] # count
+         for Scon in [10] # count
          ],
          "ltspice/coilgunauto2.asc")
 """
+#"""
 fullsim([Coilsim(Wdia, Bwid, Bhei, Lthi, Turn, Clen, Capa, Volt, Scon)
-         for Wdia in [awg(i) for i in [12]] # mm (+ AWG)
+         for Wdia in [awg(16)] # mm (+ AWG)
+         for Bwid in [24] # mm
+         for Bhei in [3] # mm
+         for Lthi in [3] # mm
+         for Turn in [120] # count
+         for Clen in [50] # mm
+         for Capa in [10000] # uF
+         for Volt in [90] # V
+         for Scon in [10] # count
+         ],
+         "ltspice/coilgunauto2.asc")
+#"""
+"""
+fullsim([Coilsim(Wdia, Bwid, Bhei, Lthi, Turn, Clen, Capa, Volt, Scon)
+         for Wdia in [awg(12)] # mm (+ AWG)
          for Bwid in [24] # mm
          for Bhei in [3] # mm
          for Lthi in [2] # mm
          for Turn in [75] # count
          for Clen in [40] # mm
-         for Capa in [1000,10000] # uF
-         for Volt in [63,90] # V
+         for Capa in [1000] # uF
+         for Volt in [63] # V
          for Scon in [20] # count
          ],
          "ltspice/coilgunauto2.asc")
-"""
+         """
